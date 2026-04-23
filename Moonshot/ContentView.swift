32349f7c2e042443b6f9c92925fcc64a7f4d4218
Edwin Cardenas <eacardenase@gmail.com>
2026-04-23 15:21:32 -0500

    feat: using and resizing images

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(.witchHatAtelier)
+                .resizable()
+                .scaledToFit()
+                .containerRelativeFrame(.horizontal) { size, axis in
+                    size * 0.8
+                }
         }
-        .padding()
     }
 }
 

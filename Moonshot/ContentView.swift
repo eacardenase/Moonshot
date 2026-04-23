0bf177dadf02fb55db0f687841b4994b1cb826e3
Edwin Cardenas <eacardenase@gmail.com>
2026-04-23 15:31:41 -0500

    feat: scroll views and vertical stack

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,16 +7,32 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    let text: String
+
+    var body: some View {
+        Text(text)
+    }
+
+    init(_ text: String) {
+        print("Creating a new CustomText")
+
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(.witchHatAtelier)
-                .resizable()
-                .scaledToFit()
-                .containerRelativeFrame(.horizontal) { size, axis in
-                    size * 0.8
+        ScrollView {
+            VStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText("Item \($0 + 1)")
+                        .font(.title)
                 }
+            }
+            .frame(maxWidth: .infinity)
         }
+        .scrollIndicators(.never)
     }
 }
 

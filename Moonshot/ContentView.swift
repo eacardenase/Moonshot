21e7bbb8039898d51e63717373cf5c749402d870
Edwin Cardenas <eacardenase@gmail.com>
2026-04-23 20:44:32 -0500

    feat: style up the ui

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,6 +7,21 @@
 
 import SwiftUI
 
+struct MissionView: View {
+    let mission: Mission
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                ForEach(mission.crew, id: \.name) { crewMember in
+                    Text(crewMember.name)
+                }
+            }
+            .navigationTitle(mission.displayName)
+        }
+    }
+}
+
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
@@ -17,31 +32,44 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
-                LazyVGrid(columns: layout) {
+                LazyVGrid(columns: layout, spacing: 16) {
                     ForEach(missions) { mission in
                         NavigationLink {
-                            Text("Detail view")
+                            MissionView(mission: mission)
                         } label: {
                             VStack {
                                 Image(mission.imageName)
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 100, height: 100)
+                                    .padding()
 
                                 VStack {
                                     Text(mission.displayName)
                                         .font(.headline)
+                                        .foregroundStyle(.white)
+
                                     Text(mission.formattedLaunchDate)
                                         .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.5))
                                 }
+                                .padding(.vertical)
                                 .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
                             }
-                            .foregroundStyle(.primary)
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            }
                         }
                     }
                 }
+                .padding([.horizontal, .bottom])
             }
             .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
         }
     }
 }
